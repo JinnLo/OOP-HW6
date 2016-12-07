bf035d8e72b70a2d650feff388b4fd8bc75cacc9
user <user@140.134.248.26>
2016-12-07 22:38:24 +0800

    add String player

--- src/fcu/iecs/oops/hw6/Player.java
+++ src/fcu/iecs/oops/hw6/Player.java
@@ -1,5 +1,35 @@
 package fcu.iecs.oops.hw6;
 
 public class Player {
+	
+	private String playerName;
+	private Pokemon pokemons[];
+	private int level = 1;
+	public Player(String playerName) {
+		super();
+		this.playerName = playerName;
+	
+	}
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public Pokemon[] getPokemons() {
+		return pokemons;
+	}
+	public void setPokemons(Pokemon[] pokemons) {
+		this.pokemons = pokemons;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level += level;
+	}
+	
+	
+	
 
 }
